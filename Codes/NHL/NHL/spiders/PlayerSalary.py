5eb4726181e41b2d56747f9fede857c53c0cb670
André Bastos <andrebastos1198@gmail.com>
2018-10-18 14:13:48 -0300

    commit

--- Codes/NHL/NHL/spiders/PlayerSalary.py
+++ Codes/NHL/NHL/spiders/PlayerSalary.py
@@ -1,6 +1,5 @@
 import scrapy
 
-
 class PlayersalarySpider(scrapy.Spider):
     name = 'PlayerSalary'
     REDIRECT_MAX = 50
@@ -8,13 +7,13 @@ class PlayersalarySpider(scrapy.Spider):
     def start_requests(self):
         url = ['https://www.spotrac.com/nhl/rankings/']
         teams = ['anaheim-ducks', 'arizona-coyotes', 'boston-bruins', 'buffalo-sabres', 'calgary-flames', 'carolina-hurricanes', 'chicago-blackhawks', 'colorado-avalanche',
-                 'columbus-blue-jackets', 'dallas-stars', 'detroit-red-wings', 'edmonton-oilers', 'florida-panthers', 'los-angeles-kings', 'minnesota-wild', 'montreal-canadiens',
-                 'nashville-predators', 'new-jersey-devils', 'new-york-islanders', 'new-york-rangers', 'ottawa-senators', 'philadelphia-flyers', 'pittsburgh-penguins', 'san-jose-sharks',
-                 'st-louis-blues', 'tampa-bay-lightning', 'toronto-maple-leafs', 'vancouver-canucks', 'vegas-golden-knights', 'washington-capitals', 'winnipeg-jets', 'atlanta-thrashers',
-                 'phoenix-coyotes']
+                'columbus-blue-jackets', 'dallas-stars', 'detroit-red-wings', 'edmonton-oilers', 'florida-panthers', 'los-angeles-kings', 'minnesota-wild', 'montreal-canadiens',
+                'nashville-predators', 'new-jersey-devils', 'new-york-islanders', 'new-york-rangers', 'ottawa-senators', 'philadelphia-flyers', 'pittsburgh-penguins', 'san-jose-sharks',
+                'st-louis-blues', 'tampa-bay-lightning', 'toronto-maple-leafs', 'vancouver-canucks', 'vegas-golden-knights', 'washington-capitals', 'winnipeg-jets', 'atlanta-thrashers',
+                'phoenix-coyotes']
 
         for i in range(2000, 2018):
-            for j in range(32):
+            for j in range(33):
                 send = url[0] + str(i) + '/' + teams[j]
 
                 yield scrapy.Request(url = send, meta={'team' : teams[j]}, callback = self.parse)
